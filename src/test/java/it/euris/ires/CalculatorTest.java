56975ae22eb3306b1e2d17c4e7419f0ed6ff25c0
git-agne <ag.devetta@gmail.com>
2021-07-16 13:02:43 +0200

    Test parametrici

--- src/test/java/it/euris/ires/CalculatorTest.java
+++ src/test/java/it/euris/ires/CalculatorTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CalculatorTest {
 
@@ -22,7 +24,7 @@ public class CalculatorTest {
         // arrange
         int numbA = 1;
         int numbB = 2;
-        int expectedRes = 2;
+        int expectedRes = 3;
 
         // act
         int result = c.add(numbA, numbB);
@@ -39,6 +41,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("2 * 3 = 6")
     void givenMultWhenInput2AndInput3ThenReturn6() {
         // arrange
         int a = 2;
@@ -53,6 +56,7 @@ public class CalculatorTest {
     }
 
     @Test
+    @DisplayName("4 / 2 = 2")
     void givenDivWhenInput4AndInput2ThenReturn2() {
         // arrange
         int a = 4;
@@ -64,5 +68,52 @@ public class CalculatorTest {
 
         // assertion
         assertEquals(expectedRes, res, String.format("%d * %d should be 2", a, b));
+    }
+
+    @ParameterizedTest(name = "GivenFirstArgument {0} AndSecondArgument {1} WhenAddThenShouldReturn {2}")
+    @CsvSource({
+            "0, 1, 1",
+            "0, -1, -1",
+            "-50, 50, 0",
+            Integer.MAX_VALUE + ", " + Integer.MAX_VALUE + ", -2"})
+    void addWithBvaValues(int first, int second, int expectedResult) {
+        Calculator c = new Calculator();
+        assertEquals(expectedResult, c.add(first, second), () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @ParameterizedTest(name = "GivenFirstArgument {0} AndSecondArgument {1} WhenSubThenShouldReturn {2}")
+    @CsvSource({
+            "0, 1, -1",
+            "0, -1, 1",
+            "-50, 50, -100",
+            Integer.MAX_VALUE + ", " + Integer.MAX_VALUE + ", 0"
+    })
+    void subWithBvaValues(int first, int second, int expectedResult) {
+        int result = c.sub(first, second);
+        assertEquals(expectedResult, result, () -> first + " - " + second + " should equal " + expectedResult);
+    }
+
+    @ParameterizedTest(name = "GivenFirstArgument {0} AndSecondArgument {1} WhenMultThenShouldReturn {2}")
+    @CsvSource({
+            "0, 1, null",
+            "1, 2, 2",
+            "50, 20, 1000",
+            Integer.MAX_VALUE + ", " + "1, " + Integer.MAX_VALUE
+    })
+    void multWithBvaValues(int first, int second, int expectedResult) {
+        int result = c.mult(first, second);
+        assertEquals(expectedResult, result, () -> first + " + " + second + " should equal " + expectedResult);
+    }
+
+    @ParameterizedTest(name = "GivenFirstArgument {0} AndSecondArgument {1} WhenDivThenShouldReturn {2}")
+    @CsvSource({
+            "0, 1, null",
+            "1, 2, 2",
+            "50, 20, 1000",
+            Integer.MAX_VALUE + ", " + Integer.MIN_VALUE + ", 0"
+    })
+    void divWithBvaValues(int first, int second, int expectedResult) {
+        int result = c.div(first, second);
+        assertEquals(expectedResult, result, () -> first + " + " + second + " should equal " + expectedResult);
     }
 }
